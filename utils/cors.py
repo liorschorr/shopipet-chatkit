4b2f636dd7bf503755fadf95668dc34c7b6090f4
liorschorr <110768878+liorschorr@users.noreply.github.com>
2025-12-11 18:16:15 +0200

    Add CORS headers function

--- utils/cors.py
+++ utils/cors.py
@@ -1,1 +1,6 @@
-
+def cors_headers():
+    return {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type"
+    }
